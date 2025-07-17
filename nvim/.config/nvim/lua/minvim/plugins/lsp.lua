8b7744625d26fb1993f1c2baf6c3e27c6fabca44
su5kk <36619221+su5kk@users.noreply.github.com>
2025-07-17 13:28:29 +0300

    nvim: [cmp] <cr> to confirm

--- nvim/.config/nvim/lua/minvim/plugins/lsp.lua
+++ nvim/.config/nvim/lua/minvim/plugins/lsp.lua
@@ -85,7 +85,10 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<CR>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Insert,
+                    select = false,
+                }),
                 ['<C-e>'] = vim.NIL
             }),
 
