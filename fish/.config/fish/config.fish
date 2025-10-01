e700433a2ef45e82c803f87e299a440b6eab86c3
Temirlan Tursunov <temirlantursunov@Temirlans-MacBook-Pro.local>
2025-10-01 16:26:07 +0300

    nvim: mini pick and sidekick

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,4 @@
 set --export EDITOR /opt/homebrew/bin/nvim
+
+# opencode
+fish_add_path /Users/temirlantursunov/.opencode/bin
--- nvim/.config/nvim/lua/minvim/plugins/mini-pick.lua
+++ nvim/.config/nvim/lua/minvim/plugins/mini-pick.lua
@@ -4,9 +4,8 @@ return {
     config = function()
         require("mini.pick").setup()
         vim.keymap.set('n', '<leader>ff', '<cmd>Pick files<CR>', { desc = 'List functions' })
-        vim.keymap.set('n', '<leader>fg', '<cmd>Pick grep_live<CR>', { desc = 'List functions' })
+        vim.keymap.set('n', '<leader>fg', '<cmd>Pick grep<CR>', { desc = 'List functions' })
         vim.keymap.set('n', '<leader>fb', '<cmd>Pick buffers<CR>', { desc = 'List functions' })
-        vim.keymap.set('n', '<leader>fb', '<cmd>Pick diagnostic<CR>', { desc = 'List functions' })
-        vim.keymap.set('n', 'gr', '<cmd>Pick lsp scope=\'references\'<CR>', { desc = 'List functions' })
+        vim.keymap.set('n', '<leader>lq', '<cmd>Pick diagnostic<CR>', { desc = 'List functions' })
     end
 }
