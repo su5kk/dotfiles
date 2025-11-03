13859a39c26c16db28c2f2cef336750ab55cfd8a
Temirlan Tursunov <temirlantursunov@ip-192-168-1-179.eu-central-1.compute.internal>
2025-11-03 20:32:22 +0200

    better navigation

--- nvim/.config/nvim/lua/minvim/keys.lua
+++ nvim/.config/nvim/lua/minvim/keys.lua
@@ -1,8 +1,16 @@
 -- general
 vim.g.mapleader = " "
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
-vim.keymap.set({ "n", "v" }, "<leader>d", '"+d')
-vim.keymap.set("n", "<leader>tf", ':terminal<cr>')
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
-vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { desc = 'List functions' })
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'List functions' })
+local map = vim.keymap.set
+map({ "n", "v" }, "<leader>y", '"+y')
+map({ "n", "v" }, "<leader>d", '"+d')
+map("n", "<leader>tf", ':terminal<cr>')
+map('t', '<Esc>', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+map('n', '<leader>la', vim.lsp.buf.code_action, { desc = 'List functions' })
+map('n', 'gr', "<cmd>lua MiniExtra.pickers.lsp({ scope = 'references' })<cr>", { desc = 'List functions' })
+map('n', 'gT', "<cmd>lua MiniExtra.pickers.lsp({ scope = 'type_definition' })<cr>", { desc = 'List functions' })
+
+map({ "n", "t" }, "<Leader>tn", "<Cmd>tabnew<CR>")
+map({ "n", "t" }, "<Leader>tx", "<Cmd>tabclose<CR>")
+for i = 1, 8 do
+	map({ "n", "t" }, "<Leader>" .. i, "<Cmd>tabnext " .. i .. "<CR>")
+end
